76db7fc66d09fd84683fb99dc3e0b467c0cef6c1
Caio Burton <101891262+CaioBurton@users.noreply.github.com>
2024-09-04 11:59:54 -0300

    Fonts foram caregadas no _layout.jsx
    Exemplo de uso no index.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Stack } from 'expo-router';
+import React, { useEffect } from 'react';
+import { View, Image, Text } from 'react-native';
+import { useFonts } from 'expo-font';
+import { icons, images } from '../constants';
+import { styled } from 'nativewind';
+import { SplashScreen, Stack } from 'expo-router';
 
-export default function RootLayout() {
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    'Montserrat-Thin': require('../assets/fonts/Montserrat-Thin.ttf'),
+    'Montserrat-ExtraLight': require('../assets/fonts/Montserrat-ExtraLight.ttf'),
+    'Montserrat-Light': require('../assets/fonts/Montserrat-Light.ttf'),
+    'Montserrat-Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-SemiBold': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+    'Montserrat-Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-ExtraBold': require('../assets/fonts/Montserrat-ExtraBold.ttf'),
+    'Montserrat-Black': require('../assets/fonts/Montserrat-Black.ttf'),
+    'Poppins-Thin': require('../assets/fonts/Poppins-Thin.ttf'),
+    'Poppins-ExtraLight': require('../assets/fonts/Poppins-ExtraLight.ttf'),
+    'Poppins-Light': require('../assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-ExtraBold': require('../assets/fonts/Poppins-ExtraBold.ttf'),
+    'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ headerShown: false}}
-      />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
-}
+};
 
+export default RootLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,3 @@
-// C:\Users\Caio Burton\Desktop\DFUApp\App.js
-
 import React from 'react';
 import { View, Image, Text } from 'react-native';
 import { icons, images } from '../constants';
@@ -8,7 +6,7 @@ import { styled } from 'nativewind';
 const App = () => {
   return (
     <View className="flex-1 justify-center items-center bg-blue-50">
-      <Text className="text-2xl font-psemibold text-gray-900 mb-4">
+      <Text className="text-3xl font-msemibold text-gray-900 mb-4">
         Welcome to DFUApp
       </Text>
       <Image source={images.logo} className="w-24 h-24 mb-4" />
